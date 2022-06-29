4aed3598df84c249bf09135d489e2433f5e28f1a
NilsOveTen <github.nils.ove@tendenes.com>
2022-06-29 09:05:44 +0200

    chore: replace deprecated security config

--- src/main/kotlin/no/fdk/records_of_processing_activities/configuration/SecurityConfig.kt
+++ src/main/kotlin/no/fdk/records_of_processing_activities/configuration/SecurityConfig.kt
@@ -2,37 +2,32 @@ package no.fdk.records_of_processing_activities.configuration
 
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.oauth2.jwt.JwtClaimNames.AUD
+import org.springframework.security.web.SecurityFilterChain
 
-@Configuration
-open class SecurityConfig : WebSecurityConfigurerAdapter() {
-    override fun configure(http: HttpSecurity) {
+@EnableWebSecurity
+open class SecurityConfig {
+
+    @Bean
+    open fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.csrf().disable()
-
-        http.cors()
-            .and()
-                .authorizeRequests()
-                    .antMatchers(HttpMethod.OPTIONS)
-                        .permitAll()
-                    .antMatchers(HttpMethod.GET, "/ping")
-                        .permitAll()
-                    .antMatchers(HttpMethod.GET, "/ready")
-                        .permitAll()
-                    .anyRequest()
-                        .authenticated()
-            .and()
-                .oauth2ResourceServer()
-                    .jwt()
+            .cors().and()
+            .authorizeRequests{ authorize ->
+                authorize.antMatchers(HttpMethod.OPTIONS).permitAll()
+                    .antMatchers(HttpMethod.GET, "/ping").permitAll()
+                    .antMatchers(HttpMethod.GET, "/ready").permitAll()
+                    .anyRequest().authenticated() }
+            .oauth2ResourceServer { resourceServer -> resourceServer.jwt() }
+        return http.build()
     }
 
     @Bean
-    open fun jwtDecoder(properties: OAuth2ResourceServerProperties): JwtDecoder? {
+    open fun jwtDecoder(properties: OAuth2ResourceServerProperties): JwtDecoder {
         val jwtDecoder = NimbusJwtDecoder.withJwkSetUri(properties.jwt.jwkSetUri).build()
         jwtDecoder.setJwtValidator(
                 DelegatingOAuth2TokenValidator(
