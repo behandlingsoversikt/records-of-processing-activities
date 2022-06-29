b1e3d611781775a8604df85898073566bd66aeae
NilsOveTen <github.nils.ove@tendenes.com>
2022-06-29 09:05:44 +0200

    chore: replace deprecated security config

--- src/main/kotlin/no/fdk/records_of_processing_activities/Application.kt
+++ src/main/kotlin/no/fdk/records_of_processing_activities/Application.kt
@@ -2,8 +2,11 @@ package no.fdk.records_of_processing_activities
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
+@EnableConfigurationProperties(OAuth2ResourceServerProperties::class)
 open class Application
 
 fun main(args: Array<String>) {
