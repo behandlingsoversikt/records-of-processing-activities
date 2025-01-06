399d7243f2cea7b657cc28288f6d3c1d3f03a93f
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-01-06 15:39:10 +0100

    chore: cors config

--- src/main/kotlin/no/fdk/records_of_processing_activities/configuration/SecurityConfig.kt
+++ src/main/kotlin/no/fdk/records_of_processing_activities/configuration/SecurityConfig.kt
@@ -1,28 +1,45 @@
 package no.fdk.records_of_processing_activities.configuration
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.oauth2.jwt.JwtClaimNames.AUD
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
-open class SecurityConfig {
+open class SecurityConfig(@Value("\${application.cors.originPatterns}") private val corsOriginPatterns: Array<String>) {
 
     @Bean
     open fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.csrf().disable()
-            .cors().and()
-            .authorizeHttpRequests{ authorize ->
-                authorize.requestMatchers(HttpMethod.OPTIONS).permitAll()
-                    .requestMatchers(HttpMethod.GET, "/ping").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/ready").permitAll()
-                    .anyRequest().authenticated() }
-            .oauth2ResourceServer { resourceServer -> resourceServer.jwt() }
+        http {
+            cors {
+                configurationSource = CorsConfigurationSource {
+                    val config = CorsConfiguration()
+                    config.allowCredentials = false
+                    config.allowedHeaders = listOf("*")
+                    config.maxAge = 3600L
+                    config.allowedOriginPatterns = corsOriginPatterns.toList()
+                    config.allowedMethods = listOf("GET", "POST", "OPTIONS", "DELETE", "PATCH")
+                    config
+                }
+            }
+            authorizeHttpRequests {
+                authorize(HttpMethod.OPTIONS, "/**", permitAll)
+                authorize(HttpMethod.GET, "/ping", permitAll)
+                authorize(HttpMethod.GET, "/ready", permitAll)
+                authorize(anyRequest, authenticated)
+            }
+            csrf { disable() }
+            oauth2ResourceServer { jwt { } }
+        }
         return http.build()
     }
 
